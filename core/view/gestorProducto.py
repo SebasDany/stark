024012d5a27e370b5c1498e68e3223ea38a30f7d
SebasDany <sguandinangod@est.ups.edu.ec>
2021-07-24 00:01:54 -0500

    Validacion de las busquedas  e implementacion de la pantalla de resultados

--- core/view/gestorProducto.py
+++ core/view/gestorProducto.py
@@ -1,7 +1,8 @@
-from core.models import Detalle_importacion, Factura_proveedor, Mercancia
+from core.models import Detalle_importacion, Factura_proveedor, Mercancia, Proveedor
 from ..controlador import  buscarSKU, guardarProductoImport,saveDetalleImportacion
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import numpy
 
 def iniciarProduct(request):
     dtp=Detalle_importacion()
@@ -45,7 +46,20 @@ def addProductImport(request,id,idas,idfa):
 def viewProduct(request,id,idas,idfa):
     pr=Detalle_importacion.objects.filter(importacion=id)
  
-    proveedores=Factura_proveedor.objects.filter(importacion=id)
+    proveedores=Factura_proveedor.objects.filter(importacion=id).distinct()
+    # print(len(proveedores))
+    # prov=[]
+    # for i in range(len(proveedores)):
+    #     prov.append(proveedores[i].proveedor.id)
+
+    # unicos=numpy.unique(prov)
+    # print( numpy.unique(prov))
+    # h=[]
+    # for j in range(len(prov)-1):
+    #     print(unicos[j])
+    #     k=Proveedor.objects.get(id=unicos[j])
+    #     h.append(k)
+
     mercancias=Mercancia.objects.select_related().all()
     datos={
                 "id":id,
@@ -61,35 +75,35 @@ def viewProduct(request,id,idas,idfa):
 
 def productosImportados(request,id,idas,idfa):
     if request.method=='POST':
-        if "id_producto" in request.POST:
-            product_id=request.POST.getlist('id_producto')  
-            id_df=request.POST.getlist('id_df') 
-            print(product_id)
-            print(id_df)
-            precio=request.POST.getlist('precio')
-            proveedor=request.POST.getlist('proveedor')
-            cantidad=request.POST.getlist('cantidad')
-            
-            mercancia=request.POST.getlist('mercancia')
-            peso=request.POST.getlist('peso')
+        
+        product_id=request.POST.getlist('id_producto')  
+        id_df=request.POST.getlist('id_df') 
+        print(product_id)
+        print(id_df)
+        precio=request.POST.getlist('precio')
+        proveedor=request.POST.getlist('proveedor')
+        cantidad=request.POST.getlist('cantidad')
+        
+        mercancia=request.POST.getlist('mercancia')
+        peso=request.POST.getlist('peso')
 
-            saveDetalleImportacion(id,id_df,peso,precio,cantidad,product_id,mercancia,proveedor)
-            pr=Detalle_importacion.objects.filter(importacion=id)
-    
-            proveedores=Factura_proveedor.objects.filter(importacion=id)
-            mercancias=Mercancia.objects.select_related().all()
-            datos={
-                    "id":id,
-                    "idas":idas,
-                    "idfa":idfa,
-                    'error':False,
-                "productos":pr,
-            
-                "proveedores":proveedores,
-                "mercancias":mercancias
-            }
+        saveDetalleImportacion(id,id_df,peso,precio,cantidad,product_id,mercancia,proveedor)
+        pr=Detalle_importacion.objects.filter(importacion=id)
 
-            return render(request, 'core/resultados.html',datos  )
+        proveedores=Factura_proveedor.objects.filter(importacion=id)
+        mercancias=Mercancia.objects.select_related().all()
+        datos={
+                "id":id,
+                "idas":idas,
+                "idfa":idfa,
+                'error':False,
+            "productos":pr,
+        
+            "proveedores":proveedores,
+            "mercancias":mercancias
+        }
+
+        return render(request, 'core/resultados.html',datos  )
 
        
     return HttpResponse("<h1>"+"</h>")

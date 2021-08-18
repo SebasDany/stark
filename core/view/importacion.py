e31eb952889299d2f727c5e23efb8ac6970123cf
SebasDany <sguandinangod@est.ups.edu.ec>
2021-08-17 21:55:34 -0500

    correcciones generales

--- core/view/importacion.py
+++ core/view/importacion.py
@@ -2,14 +2,11 @@
 from core.view.gestorProducto import saveMercacia
 from django.shortcuts import render, redirect
 from ..utilidades import  crearH
-from ..models import Factura_proveedor,Importacion, Mercancia, Producto,Proveedor
+from ..models import Detalle_importacion, Factura_proveedor,Importacion, Mercancia, Producto,Proveedor
 from ..forms import UserRegisterForm, ProductRegister
 from django.contrib import messages
 import datetime
-por_advalorem=[0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-subpartida=['8542390000','9031809000','9025900000', '3926909000', '8518909090', '9006910000', '8531200000', '8536419000', '8544429000', '9032891100', '8517692000', '8543709000', '8542310000', '8541900000', '8541100000', '8542900000', '8536690090', '8533319000']
-mercancia=['TARJETAS ELECTRÓNICAS','SENSORES','SENSORES MEDIDOR D', 'Carcasa De Plastico', 'Sensor De Microfono', 'PARTE DE CAMARA', 'Mini Pantalla Display', 'RELE', 'CABLES', 'REGULADORES DE VOLTAJE', 'RECEPTOR INALAMBRICO', 'KIT DE ROBOTICA', 'CIRCUITOS INTEGRADOS', 'PORTA LED', 'DIODOS', 'SOPORTE PARA TARJETAS', 'INTERRUPTORES', 'RESISTENCIAS']
-#saveMercacia(mercancia,subpartida,por_advalorem) #gurada la mercancia
+
 id_w=[]
 id_w.append(1)
 precio_compra=[]
@@ -24,13 +21,19 @@ observaciones=[]
 def startImport(request):
     imp=Importacion(fecha=str(datetime.datetime.today()).split()[0],descripcion="",tipo="",origen="",estado=0)
     imp.save()
+    
+    if len(Mercancia.objects.all())==0:
+        por_advalorem=[0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        subpartida=['8542390000','9031809000','9025900000', '3926909000', '8518909090', '9006910000', '8531200000', '8536419000', '8544429000', '9032891100', '8517692000', '8543709000', '8542310000', '8541900000', '8541100000', '8542900000', '8536690090', '8533319000']
+        mercancia=['TARJETAS ELECTRÓNICAS','SENSORES','SENSORES MEDIDOR D', 'Carcasa De Plastico', 'Sensor De Microfono', 'PARTE DE CAMARA', 'Mini Pantalla Display', 'RELE', 'CABLES', 'REGULADORES DE VOLTAJE', 'RECEPTOR INALAMBRICO', 'KIT DE ROBOTICA', 'CIRCUITOS INTEGRADOS', 'PORTA LED', 'DIODOS', 'SOPORTE PARA TARJETAS', 'INTERRUPTORES', 'RESISTENCIAS']
+        saveMercacia(mercancia,subpartida,por_advalorem) #gurada la mercancia
     id_impor=Importacion.objects.last()
     id=id_impor.id
     crearH(id,0,0,0)
-    for i in Mercancia.objects.all():
-        print(i.id, i.nombre)
-    for i in Producto.objects.all():
-        print(i.id, i.nombre)
+    # for i in Mercancia.objects.all():
+    #     #print(i.id, i.nombre)
+    # for i in Producto.objects.all():
+    #     print(i.id, i.nombre)
     return redirect('importacion',id)
 
 # Create your views here.
@@ -100,8 +103,8 @@ def importacion(request,id):
         importacion=Importacion.objects.get(id=id)
         fac=Factura_proveedor.objects.filter(importacion=id)
         if(len(fac) < int(cantidad)):
-            print(len(fac),int(cantidad))
-            print(len(fac)-int(cantidad))
+            #print(len(fac),int(cantidad))
+            #print(len(fac)-int(cantidad))
         
             for k in  range(int(cantidad)-len(fac)):
                 pf=Factura_proveedor(proveedor=proveedor,importacion=importacion,num_cajas=0,valor_factura=0,valor_envio=0,comision_envio=0,isd=0,total_pago=0,extra=0)  
